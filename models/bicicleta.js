6e844192a3dd75f09334105777262b0a406fd458
Victor Sanchez <victor_sanchez@outlook.com>
2020-09-01 17:48:29 -0300

    add CRUD

---
+++ models/bicicleta.js
@@ -0,0 +1,42 @@
+var Bicicleta = function (id, color, modelo, ubicacion){
+    this.id = id;
+    this.color = color;
+    this.modelo = modelo;
+    this.ubicacion = ubicacion;
+}
+
+Bicicleta.prototype.toString = function (){
+    return "id: " + this.id + " | color: " + this.color;
+}
+
+Bicicleta.allBicis = [];
+Bicicleta.add = function(aBici){
+    Bicicleta.allBicis.push(aBici);
+}
+
+Bicicleta.findById = function(aBiciId){
+    var aBici = Bicicleta.allBicis.find(x => x.id == aBiciId);
+    if (aBici)
+        return aBici;
+    else
+        throw new Error(`No existe una bicicleta con el id ${aBiciId}`);
+}
+
+Bicicleta.removeById = function(aBiciId){
+    // Bicicleta.findById(aBiciId);
+    for(var i = 0; i < Bicicleta.allBicis.length; i++){
+        if(Bicicleta.allBicis[i].id == aBiciId){
+            Bicicleta.allBicis.splice(i, 1);
+            break;
+        }
+    }
+}
+
+//Bicicletas 
+var a = new Bicicleta (1, "Rojo", "Urbana", [-27.4689004, -58.8312304]);
+var b = new Bicicleta (2, "Azul", "Urbana", [-27.4887014, -58.8212314]);
+
+Bicicleta.add(a);
+Bicicleta.add(b);
+
+module.exports = Bicicleta;
