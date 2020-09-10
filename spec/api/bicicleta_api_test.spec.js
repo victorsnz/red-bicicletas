32e773ed6d946ee0ad19ffbd5a8912e33476ae83
Victor Sanchez <victor_sanchez@outlook.com>
2020-09-10 02:20:51 -0300

    added: update and delete in test

--- spec/api/bicicleta_api_test.spec.js
+++ spec/api/bicicleta_api_test.spec.js
@@ -3,6 +3,9 @@ var request = require('request');
 var server = require('../../bin/www');
 
 
+//Se ejecuta antes de cada funcion
+beforeEach(() => { Bicicleta.allBicis = []; });
+
 describe('Bicicleta API', () => {
     describe('GET BICICELTAS /', () => {
         it('Status 200', () => {
@@ -20,8 +23,10 @@ describe('Bicicleta API', () => {
 
 describe('POST BICICLETAS /create', () => {
     it('STATUS 200', (done) => {
+        expect(Bicicleta.allBicis.length).toBe(0);
+
         var headers = {'content-type':'application/json'};
-        var aBici = '{"id": 10, "color": "Azul", "modelo": "Urbana", "lat": -27, "lng": -58}';
+        var aBici = '{"id": 10, "color": "rojo", "modelo": "Urbana", "lat": -27, "lng": -58}';
         request.post({
             headers: headers,
             url: 'http://localhost:3000/api/bicicletas/create',
@@ -29,7 +34,52 @@ describe('POST BICICLETAS /create', () => {
             }, function(error, response, body){
                 expect(response.statusCode).toBe(200);
                 expect(Bicicleta.findById(10).color).toBe("rojo");
-                done;
+            
+                done();
+        });
+    });
+});
+
+describe('POST BICICLETAS /update', () => {
+    it('STATUS 200', (done) => {
+        expect(Bicicleta.allBicis.length).toBe(0);
+
+        let aBici = new Bicicleta(10, "Azul", "Urbana", [-27, -58]);
+        Bicicleta.add(aBici);
+
+        var headers = { 'content-type': 'application/json' };
+        aBici = '{"id": 10, "color": "Negro", "modelo": "Urbana", "lat": -27, "lng": -58}';
+        request.post({
+            headers: headers,
+            url: 'http://localhost:3000/api/bicicletas/update',
+            body: aBici
+        }, function (error, response, body) {
+            expect(response.statusCode).toBe(200);
+            expect(Bicicleta.findById(10).color).toBe("Negro");
+            
+            done();
+        });
+    });
+});
+
+describe('DELETE BICICLETAS /delete', () => {
+    it('STATUS 200', (done) => {
+        expect (Bicicleta.allBicis.length).toBe(0);
+
+        var aBici = new Bicicleta(10, "Azul", "Urbana", [-27, -58]);
+        Bicicleta.add(aBici);
+
+        var headers = { 'content-type': 'application/json' };
+        
+        var code = '{"code":10}';
+    
+        request.delete({
+            headers: headers,
+            url: 'http://localhost:3000/api/bicicletas/delete',
+            body: code
+        }, function (error, response, body) {
+            expect(response.statusCode).toBe(204);
+            done();
         });
     });
 });
