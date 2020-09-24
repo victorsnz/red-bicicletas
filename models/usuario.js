1f2864e051c8e1b3812c5bf79b1f6cca99675f2f
Victor Sanchez <victor_sanchez@outlook.com>
2020-09-24 00:58:07 -0300

    add authentication

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,55 @@
 var mongoose = require('mongoose');
 var Reserva = require('./reserva');
+const bcrypt = require('bcrypt');
+const uniqueValidator = require('mongoose-unique-validator');
+
 var Schema = mongoose.Schema;
+const saltRounds = 10;
+
+const validateEmail = function(email){
+    const re = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    return re.test(email);
+};
 
 var usuarioSchema = new Schema({
-    nombre: String,
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es Obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        unique: true,
+        validate: [validateEmail, 'Por favor, ingrese un email válido'],
+        match: [ /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
 });
+
+usuarioSchema.plugin(uniqueValidator, {message:'El {PATH} ya existe con otro usuario.'});
+
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+});
+
+usuarioSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.password);
+}
 
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb){
     var reserva = new Reserva({usuario: this._id, bicicleta:biciId, desde: desde, hasta: hasta});
