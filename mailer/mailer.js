2badb314d3c076153dc142c5e55dcb5e2705e933
Victor Sanchez <victor_sanchez@outlook.com>
2020-10-12 01:17:40 -0300

    sendgrid added

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,43 @@
 const nodemailer = require('nodemailer');
+const sgTransport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-  host: "smtp.ethereal.email",
-  port: 587,
-  auth: {
-    user: "vada4@ethereal.email",
-    pass: "TFx9HQn8U7vvvxet1B",
-  },
-};
+let mailConfig;
+if(process.env.NODE_ENV === 'production'){
+  const options = {
+    auth: {
+      api_key: process.env.SENDGRID_API_SECRET
+    }
+  }
+  mailConfig = sgTransport(option);
+}else{
+  if(process.env.NODE_ENV === 'staging'){
+    console.log('XXXXXXXXXXX');
+    const options = {
+      auth: {
+        api_key: process.env.SENDGRID_API_SECRET
+      }
+    }
+    mailConfig = sgTransport(options);
+
+  } else {
+    const mailConfig = {
+      host: "smtp.ethereal.email",
+      port: 587,
+      auth: {
+        user: process.env.ehtereal_user,
+        pass: process.env.ethereal_pwd
+      },
+    };
+  }
+}
+
+// const mailConfig = {
+//   host: "smtp.ethereal.email",
+//   port: 587,
+//   auth: {
+//     user: "vada4@ethereal.email",
+//     pass: "TFx9HQn8U7vvvxet1B",
+//   },
+// };
 
 module.exports = nodemailer.createTransport(mailConfig);
