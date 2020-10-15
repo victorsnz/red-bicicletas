97567a2b24c4de5aa4474558204e06ef36ba8046
Victor Sanchez <victor_sanchez@outlook.com>
2020-10-15 01:58:56 -0300

    cambio en password usuario facebook auth

--- models/usuario.js
+++ models/usuario.js
@@ -152,7 +152,7 @@ usuarioSchema.static.FindOneOrCreateByFacebook = function findOneOrCreate(condit
                 values.email = condition.emails[0].value;
                 values.nombre = condition.displayName || 'SIN NOMBRE';
                 values.verificado = true;
-                values.password = condition._json.etag;
+                values.password = crypto.randomBytes(16).toString('hex');
                 console.log('--------------- VALUES ---------------');
                 console.log(values);
                 self.create(values, (err, result) => {
