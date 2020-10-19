956e4dd8c512ef590662d704d35dd7f114bbca48
Victor Sanchez <victor_sanchez@outlook.com>
2020-10-18 23:33:25 -0300

    bugfix auth.js

--- routes/API/auth.js
+++ routes/API/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const authAPIController = require('../../controllers/API/authControllerAPI');
 
 router.post('/authenticate', authAPIController.authenticate);
 router.post("/forgotPassword", authAPIController.forgotPassword);
-router.post('/facebook_token', passport.authenticate('facebook-token', authAPIController.authFacebookToken));
+router.post('/facebook-token', passport.authenticate('facebook-token', authAPIController.authFacebookToken));
 
 module.exports = router;
